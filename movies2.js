838b1886e9542963130a85079b986eab08bf99a5
tbtreppe <63826526+tbtreppe@users.noreply.github.com>
2021-06-01 20:10:30 -0400

    Add files via upload

--- movies2.js
+++ movies2.js
@@ -3,13 +3,12 @@ $("#form-submit").on("submit", function (evt) {
   let movie = $("#movie").val();
   let rating = $("#rating").val();
 
-  let totalMovie = `<li>movie: ${movie}, rating:${rating}</li>`;
-  let removeButton = "<button> delete </button>";
+  let $totalMovie = $(`<li>movie: ${movie}, rating:${rating}</li>`);
+  let $removeButton = $("<button> delete </button>");
 
-  $("#list-of-movies").append(totalMovie);
-  $("#list-of-movies").append(removeButton);
-  $(removeButton).on("click", function (evt) {
-    $(evt).closest("#list-of-movies").remove();
-    console.log(evt);
+  $totalMovie.append($removeButton);
+  $("#list-of-movies").append($totalMovie);
+  $removeButton.on("click", function (evt) {
+    $totalMovie.remove();
   });
 });
