aef7af079c4918977bd95799ff8a934e21f0b1be
tbtreppe <63826526+tbtreppe@users.noreply.github.com>
2021-06-14 21:57:12 -0400

    Add files via upload

---
+++ movies4.js
@@ -0,0 +1,15 @@
+$("form-submit").on("submit", function (evt) {
+  evt.preventDefault();
+
+  let movie = $("#movie").val();
+  let rating = $("#rating").val();
+  let $totalMovie = $(`<li>Movie: ${movie} Rating: ${rating}</li>`);
+  let $deletebtn = $("<button>delete</button>");
+
+  $totalMovie.append($deletebtn);
+  $("#movie-list").append($totalMovie);
+
+  $deletebtn.on("click", function () {
+    $totalMovie.remove();
+  });
+});
